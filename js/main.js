af2ea7a895013c3b520ba8f50bae5a41514e2166
Edney Ferreira Silva <edneysilva20@hotmail.com>
2020-08-20 22:40:48 -0300

    Aprendendo as funções básicas de JavaScript

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+/*
+var d = new Date;
+alert(d.getMonth()+1);
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for(count=1; count<=5; count++){
+    alert(count)
+}
+*/
+
+/* estrutura de repetição
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    //count = count + 1;
+    //count++;
+    count += 1;
+};
+*/
+
+/*através do prompt solicita informação do usuário e retorna
+var idade = prompt("Qual sua idade?");
+var idade = 18;
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/* Lista de dicionário
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/* Dicionário
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*
+var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva");
+lista.pop("maçã");
+
+console.log(lista[1]);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "))
+*/
+
+
+/*
+var nome = "Edney Silva";
+var n1 = 20;
+var n2 = 5;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem " + idade);
+alert(idade + idade2);
+alert(frase.replace("Japão", "Brasil"));
+console.log(nome);
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(frase.toLocaleLowerCase()); //Deixa tudo o elemento selecionado em caixa baixa
+console.log(frase.toUpperCase());//Deixa tudo o elemento selecionado em caixa alta
+console.log(frase.replace("Japão", "Brasil"));//Troca uma palavra escolhida
+*/
